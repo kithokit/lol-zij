c79b661becab32a27b907e1ce52490166ce729d4
Kit Ho <kithokit@mangowish.com>
2015-05-04 17:55:33 +0800

    add redis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,19 @@ class ApplicationController < ActionController::Base
 
   API_KEY="1948f104-b8fb-4787-894f-79821a2d45b0"
 
+  def redis_connection
+    if Rails.env.development?
+      return "redis://localhost:6379"
+    else
+      return ENV["REDISTOGO_URL"]
+    end
+  end
+
   def connection
     if @connection.nil?
       @connection =  Lol::Client.new API_KEY, {
         region: "na",
-        redis: "redis://localhost:6379",
+        redis: redis_connection,
         ttl: 900
       }
     end
